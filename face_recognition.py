4a81a6ebec15fba2cb0f1bdede799cd82fdf53d4
Rahul R <rahul.r4517@gmail.com>
2020-02-15 18:46:12 +0530

    new

--- face_recognition.py
+++ face_recognition.py
@@ -10,7 +10,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 # iniciate id counter
 id = 0
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Rahul', 'Karthik', 'Ilza', 'Z', 'W']
+names = ['None', 'Dennis', 'Arun', 'Rino']
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
 cam.set(3, 640)  # set video widht
